f2ee209d90dba4fed979f1a6e566b0414852ccf2
Alysson A Costa <alysson.avila.costa@gmail.com>
2018-02-13 18:19:36 +0000

    Patch migration with gem

--- lib/engine_user.rb
+++ lib/engine_user.rb
@@ -2,6 +2,14 @@ require "engine_user/version"
 
 module EngineUser
   class Engine < ::Rails::Engine
+  	isolate_namespace EngineUser
 
+  	initializer :append_migrations do |app|
+  	  unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+  	end
   end
 end
